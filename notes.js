fc942b8c893ea2b50deaeaa720cfe732e73b86e7
MothtatoSoup <maddy.puryear@outlook.com>
2025-04-22 13:16:54 -0400

    Call back hell

--- notes.js
+++ notes.js
@@ -155,6 +155,40 @@ function logRes(result){
 }
 
 // C A L L B A C K   H E L L
+// When callbacks are nested w/ in other callbacks to the point where you can't read it
+
+function task1(callback){
+    setTimeout(() => { console.log("task 1 complete")
+        callback()
+    }, 3000)
+    
+}
+function task2(callback){
+    setTimeout(() => { console.log("task 1 complete")
+        callback()
+    }, 2000)
+}
+function task3(callback){
+    setTimeout(() => { console.log("task 1 complete")
+        callback()
+    }, 2050)
+}
+function task4(callback){
+    setTimeout(() => { console.log("task 1 complete")
+        callback()
+    }, 1500)
+}
+
+task1(() => {
+    task2(() => {
+        task3(() => {
+            task4(() => {
+                console.log("All tasks complete")
+            })
+        })
+    })
+})
+
 
 
 
@@ -182,6 +216,8 @@ function f2(){
     console.log("2")
     console.log("3")
 }
+
+
 
 
 // ------------------------------------------------
